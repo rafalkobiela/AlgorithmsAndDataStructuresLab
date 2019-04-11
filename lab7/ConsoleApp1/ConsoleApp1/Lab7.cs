940d0814188da2bb3d2afb31cd87e496a06d7911
rafalkobiela <kobiela.rafal@gmial.com>
2019-04-11 22:34:15 +0200

    lab 8 task 1

--- lab7/ConsoleApp1/ConsoleApp1/Lab7.cs
+++ lab7/ConsoleApp1/ConsoleApp1/Lab7.cs
@@ -42,9 +42,11 @@ namespace Lab7
                 PathsInfo[] tmpPathInfo = { };
                 Edge[] shortestPath = { };
 
-                foreach (var start in nominatedCities)
+                for (int i = 0; i < nominatedCities.Length; i++)
+                //foreach (var start in nominatedCities)
                 {
 
+                    int start = nominatedCities[i];
 
                     double minDistTmp = double.MaxValue;
                     int minCityTmp = -1;
@@ -52,10 +54,11 @@ namespace Lab7
 
                     g_cloned.DijkstraShortestPaths(start, out PathsInfo[] d);
 
-
-                    foreach (var end in nominatedCities)
+                    for (int j = i; j < nominatedCities.Length; j++)
+                    //foreach (var end in nominatedCities)
                     {
 
+                        int end = nominatedCities[j];
                         if (start != end)
                         {
 
@@ -95,104 +98,82 @@ namespace Lab7
             }
             else /*budujemy obwodnicę*/
             {
+
                 int minCity1 = int.MaxValue;
                 int minCity2 = int.MaxValue;
                 double time = double.MaxValue;
                 Edge[] shortestPath = { };
                 int byPassCity = -1;
-                PathsInfo[] tmpTmpPathInfo = { };
                 PathsInfo[] tmpPathInfo = { };
-                int counter = 0;
+                int i1 = -1;
+                int j1 = -1;
 
-                //var pathsToAllVertices = new PathsInfo[nominatedCities.Length][];
+                //var pathsToAllVertices = new HashTable<int, PathsInfo[]>();
 
-                var pathsToAllVertices = new HashTable<int, PathsInfo[]>();
+                var pathsToAllVerticesTab = new PathsInfo[nominatedCities.Length][];
 
-                //if (g_cloned.VerticesCount == 12 * 12)
-                //{
-                //GraphExport ge = new GraphExport();
-                //ge.Export(g_cloned);
-                //}
+                for (int i = 0; i < nominatedCities.Length ; i++)
 
-                foreach (var start in nominatedCities)
                 {
-
-                    double minDistTmp = double.MaxValue;
-                    int minCityTmp = -1;
-                    Edge[] shortestPathTmp = { };
+                    int start = nominatedCities[i];
 
                     g_cloned.DijkstraShortestPaths(start, out PathsInfo[] d);
+                    //pathsToAllVertices.Insert(start, d);
+                    pathsToAllVerticesTab[i] = d;
+                    for (int j = i; j < nominatedCities.Length; j++)
 
-
-                    pathsToAllVertices.Insert(start, d);
-
-                    foreach (var end in nominatedCities)
                     {
+                        int end = nominatedCities[j];
                         if (start != end)
                         {
 
                             d[end].Dist -= passThroughCityTimes[end] / 2 + passThroughCityTimes[start] / 2;
 
-                            if (d[end].Dist < minDistTmp && d[end].Dist < time)
+                            if (d[end].Dist < time)
                             {
-                                minDistTmp = d[end].Dist;
-                                minCityTmp = end;
-                                tmpTmpPathInfo = d;
-                                //shortestPathTmp = PathsInfo.ConstructPath(start, end, d);
+                                time = d[end].Dist;
+                                minCity1 = start;
+                                minCity2 = end;
+                                tmpPathInfo = d;
+                                i1 = i;
+                                j1 = j;
                             }
-                        }
-                    }
-                    if (minCityTmp != -1)
-                    {
-                        if (time > minDistTmp)
-                        {
-                            time = minDistTmp;
-                            minCity1 = start;
-                            minCity2 = minCityTmp;
-                            tmpPathInfo = tmpTmpPathInfo;
-                            //shortestPath = shortestPathTmp;
                         }
                     }
                 }
 
-
-                foreach (var start in nominatedCities)
+                for (int i = 0; i < nominatedCities.Length; i++)
                 {
-                    double minDistTmp = double.MaxValue;
-                    int minCityTmp1 = -1;
-                    int minCityTmp2 = -1;
-                    int byPassCityTmp = -1;
+                    int start = nominatedCities[i];
 
-                    foreach (var end in nominatedCities)
+                    for (int j = i; j < nominatedCities.Length; j++)
+
                     {
-                        
-                        for (int i = 0; i < g_cloned.VerticesCount; i++)
+                        int end = nominatedCities[j];
+                        for (int ii = 0; ii < g_cloned.VerticesCount; ii++)
                         {
 
-                            if (start != end && i != start && i != end)
+                            if (start != end && ii != start && ii != end)
                             {
 
-                                double timeThroughICity = pathsToAllVertices[start][i].Dist + pathsToAllVertices[end][i].Dist - passThroughCityTimes[i] -
+                                double timeThroughICity = pathsToAllVerticesTab[i][ii].Dist + pathsToAllVerticesTab[j][ii].Dist - passThroughCityTimes[ii] -
                                        0.5 * passThroughCityTimes[start] - 0.5 * passThroughCityTimes[end];
-
-                                //if(timeThroughICity < 10)
-                                //    Console.WriteLine($"{timeThroughICity}, city{i}, c1: {start}, c2: {end} ");
 
                                 if (timeThroughICity < time && timeThroughICity >= 0)
                                 {
 
-                                    byPassCity = i;
+                                    byPassCity = ii;
                                     time = timeThroughICity;
                                     minCity1 = start;
                                     minCity2 = end;
+                                    i1 = i;
+                                    j1 = j;
                                 }
                             }
                         }
                     }
                 }
 
-                //Console.WriteLine("");
-                //Console.WriteLine($"c1: {minCity1}, c2: {minCity2}, byp: {byPassCity}");
 
                 if (time == double.MaxValue)
                 {
@@ -201,17 +182,13 @@ namespace Lab7
                 else if (byPassCity != -1)
                 {
 
-
-
-
-                    var path1 = PathsInfo.ConstructPath(minCity1, byPassCity, pathsToAllVertices[minCity1]);
-                    var path2 = PathsInfo.ConstructPath(minCity2, byPassCity, pathsToAllVertices[minCity2]);
+                    var path1 = PathsInfo.ConstructPath(minCity1, byPassCity, pathsToAllVerticesTab[i1]);
+                    var path2 = PathsInfo.ConstructPath(minCity2, byPassCity, pathsToAllVerticesTab[j1]);
                     Array.Reverse(path2);
 
-                    for(int i = 0; i < path2.Length; i++)
+                    for (int i = 0; i < path2.Length; i++)
                     {
                         path2[i] = new Edge(path2[i].To, path2[i].From);
-                        //(path2[i].To, path2[i].From) = (path2[i].From, path2[i].To);
                     }
 
                     shortestPath = new Edge[path1.Length + path2.Length];
@@ -219,30 +196,6 @@ namespace Lab7
                     Array.Copy(path2, 0, shortestPath, path1.Length, path2.Length);
 
 
-                    //foreach (var i in shortestPath)
-                    //{
-                    //    Console.Write($"{i.ToString()}, ");
-                    //}
-                    //Console.WriteLine();
-                    //Console.WriteLine($"len {shortestPath.Length}");
-
-                    //foreach (var i in g_cloned.OutEdges(byPassCity))
-                    //{
-                    //    g_cloned.ModifyEdgeWeight(byPassCity, i.To, -0.5 * passThroughCityTimes[byPassCity]);
-                    //}
-
-                    //var t1 = DateTime.Now;
-                    //g_cloned.DijkstraShortestPaths(minCity1, out PathsInfo[] d);
-                    //var t2 = DateTime.Now;
-
-
-
-                    //Console.WriteLine(t2 - t1);
-
-
-
-                    //shortestPath = PathsInfo.ConstructPath(minCity1, minCity2, d);
-                    //time = d[minCity2].Dist - passThroughCityTimes[minCity1] * 0.5 - passThroughCityTimes[minCity2] * 0.5;
                 }
                 else
                 {
---
+++ lab8/ConsoleApp1/ConsoleApp1/Lab08.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ASD.Graphs;
+
+namespace ASD
+{
+    public class Lab08 : MarshalByRefObject
+    {
+        /// <summary>
+        /// Znajduje cykl rozpoczynający się w stolicy, który dla wybranych miast,
+        /// przez które przechodzi ma największą sumę liczby ludności w tych wybranych
+        /// miastach oraz minimalny koszt.
+        /// </summary>
+        /// <param name="cities">
+        /// Graf miast i połączeń między nimi.
+        /// Waga krawędzi jest kosztem przejechania między dwoma miastami.
+        /// Koszty transportu między miastami są nieujemne.
+        /// </param>
+        /// <param name="citiesPopulation">Liczba ludności miast</param>
+        /// <param name="meetingCosts">
+        /// Koszt spotkania w każdym z miast.
+        /// Dla części pierwszej koszt spotkania dla każdego miasta wynosi 0.
+        /// Dla części drugiej koszty są nieujemne.
+        /// </param>
+        /// <param name="budget">Budżet do wykorzystania przez kandydata.</param>
+        /// <param name="capitalCity">Numer miasta będącego stolicą, z której startuje kandydat.</param>
+        /// <param name="path">
+        /// Tablica dwuelementowych krotek opisująca ciąg miast, które powinen odwiedzić kandydat.
+        /// Pierwszy element krotki to numer miasta do odwiedzenia, a drugi element decyduje czy
+        /// w danym mieście będzie organizowane spotkanie wyborcze.
+        /// 
+        /// Pierwszym miastem na tej liście zawsze będzie stolica (w której można, ale nie trzeba
+        /// organizować spotkania).
+        /// 
+        /// Zakładamy, że po odwiedzeniu ostatniego miasta na liście kandydat wraca do stolicy
+        /// (na co musi mu starczyć budżetu i połączenie między tymi miastami musi istnieć).
+        /// 
+        /// Jeżeli kandydat nie wyjeżdża ze stolicy (stolica jest jedynym miastem, które odwiedzi),
+        /// to lista `path` powinna zawierać jedynie jeden element: stolicę (wraz z informacją
+        /// czy będzie tam spotkanie czy nie). Nie są wtedy ponoszone żadne koszty podróży.
+        /// 
+        /// W pierwszym etapie drugi element krotki powinien być zawsze równy `true`.
+        /// </param>
+        /// <returns>
+        /// Liczba mieszkańców, z którymi spotka się kandydat.
+        /// </returns>
+        /// 
+
+
+        int capital;
+        int currentBest = 0;
+        (int, bool)[] bestPath = new (int, bool)[0];
+        int currentCity;
+        int[] population;
+        List<int> currentPath = new List<int>();
+        Graph g;
+        bool[] isInCurrentPath;
+        double CurrentCost;
+        double budget;
+        double bestBudget;
+        int currentPopulation;
+
+        public void Print(List<int> a)
+        {
+            Console.Write("path: ");
+            foreach (var i in a)
+            {
+                Console.Write($"{i}, ");
+            }
+            Console.WriteLine();
+        }
+
+        public int ComputeElectionCampaignPath(Graph cities, int[] citiesPopulation,
+            double[] meetingCosts, double budget, int capitalCity, out (int, bool)[] path)
+        {
+
+            capital = capitalCity;
+            currentCity = capitalCity;
+            g = cities.Clone();
+            population = citiesPopulation;
+            currentPath.Add(capitalCity);
+            isInCurrentPath = new bool[g.VerticesCount];
+            CurrentCost = 0;
+            this.budget = budget;
+            bestBudget = double.MaxValue;
+            currentPopulation = population[capitalCity];
+
+            if (g.VerticesCount == 1)
+            {
+                
+                path = new (int, bool)[1] { (capital, true) };
+                
+                currentPath.Clear();
+                CurrentCost = 0;
+
+                return citiesPopulation[capital];
+                
+            }
+            else
+            {
+                bestPath = new (int, bool)[1] { (capital, true) };
+                currentBest = citiesPopulation[capital];
+                ComputeElectionCampaignPath();
+                path = bestPath;        
+            }
+            
+            //foreach(var z in g.OutEdges(2115))
+            //{
+            //    Console.Write($"{z} ");
+            //}
+            //Console.WriteLine();
+            int i = 0;
+            //g.GeneralSearchAll<EdgesStack>(v => { Console.WriteLine($"{v}, {i}"); i++; return true; }, null, null, out int cc);
+
+
+            currentPath.Clear();
+            CurrentCost = 0;
+            return currentBest;
+        }
+
+
+        
+
+        public void ComputeElectionCampaignPath()
+        {
+
+
+            int tmpCity = currentCity;
+
+
+            if (currentCity == capital && currentPath.Count > 1)
+            {
+                int sum = 0;
+
+                foreach (var c in currentPath)
+                {
+                    sum += population[c];
+                }
+
+                sum -= population[capital];
+                currentPopulation -= population[capital];
+                //Console.WriteLine($"sum: {sum}, pop: {currentPopulation}");
+                if (sum >= currentBest)
+                {
+                    
+
+                    if(!(sum == currentBest && CurrentCost >= bestBudget)){
+                        currentBest = sum;
+                        bestPath = new (int, bool)[currentPath.Count - 1];
+
+                        for (int i = 0; i < currentPath.Count - 1; i++)
+                        {
+                            bestPath[i] = (currentPath[i], true);
+                        }
+                        bestBudget = CurrentCost;
+                    }
+
+                }
+            }
+            else
+            {
+                foreach (var neigh in g.OutEdges(tmpCity))
+                {
+
+
+                    if (isInCurrentPath[neigh.To])
+                    {
+                        continue;
+                    }
+
+                    if (CurrentCost + neigh.Weight > budget)
+                    {
+                        continue;
+                    }
+
+
+                    currentPath.Add(neigh.To);
+                    isInCurrentPath[neigh.To] = true;
+                    currentCity = neigh.To;
+                    CurrentCost += neigh.Weight;
+                    currentPopulation += population[neigh.To];
+
+                    ComputeElectionCampaignPath();
+
+                    currentCity = tmpCity;
+                    currentPath.RemoveAt(currentPath.Count - 1);
+                    isInCurrentPath[neigh.To] = false;
+                    CurrentCost -= neigh.Weight;
+                    currentPopulation -= population[neigh.To];
+
+                }
+            }
+        }
+
+
+
+    }
+
+}
